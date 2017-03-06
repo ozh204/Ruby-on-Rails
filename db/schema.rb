c71599ce770149385b2e2732afdd460ed6d1a2ad
Orzech <ozh204@gmail.com>
2017-03-06 23:46:57 +0100

    poprawka bazy na heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306141723) do
+ActiveRecord::Schema.define(version: 20170306224016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20170306141723) do
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", id: false, force: :cascade do |t|
   end
 
 end
