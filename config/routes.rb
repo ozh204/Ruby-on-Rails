0be5d45666a987d8712ed08001a81a55122b4f6e
Orzech <ozh204@gmail.com>
2017-03-06 22:31:22 +0100

    dodanie logowanie

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,15 @@ Rails.application.routes.draw do
     end
   end
 
+  get  '/signup',  to: 'users#new'
   resources :users do
     collection do
       delete 'destroy_multiple'
     end
   end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end
