39981c629d51230059324698595bdd38a8b002c8
Orzech <ozh204@gmail.com>
2017-03-06 17:33:20 +0100

    dodanie rejestracji

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  # resources :waffles
-
   root 'welcome#index'
 
   resources :waffles do
+    collection do
+      delete 'destroy_multiple'
+    end
+  end
+
+  resources :users do
     collection do
       delete 'destroy_multiple'
     end
