6f23da6df0a022cef75a38bd418fd2255ba1f3b7
Orzech <ozh204@gmail.com>
2017-05-13 18:42:42 +0200

    dodanie sklepu, CRUD, modyfikacja Order by widział sklep

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513114527) do
+ActiveRecord::Schema.define(version: 20170513152930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20170513114527) do
     t.date     "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "shop_id"
+    t.index ["shop_id"], name: "index_orders_on_shop_id", using: :btree
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string   "nazwa"
+    t.string   "lokalizacja"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
